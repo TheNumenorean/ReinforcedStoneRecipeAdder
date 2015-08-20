a3dd357490caa43349ae730cabe0b22415de0d3b
Francesco Macagno <fjmacagno1@yahoo.com>
2015-08-19 23:10:31 -0700

    Backported to 1.6

--- src/main/java/net/thenumenorean/restonerecipeadder/ReStoneRecipeAdder.java
+++ src/main/java/net/thenumenorean/restonerecipeadder/ReStoneRecipeAdder.java
@@ -1,14 +1,16 @@
 package net.thenumenorean.restonerecipeadder;
 
-import ic2.api.item.IC2Items;
-import net.minecraft.init.Blocks;
+import ic2.api.item.Items;
+import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = ReStoneRecipeAdder.MODID, version = ReStoneRecipeAdder.VERSION, dependencies = "required-after:IC2;")
+@NetworkMod(clientSideRequired = true)
 public class ReStoneRecipeAdder
 {
     public static final String MODID = "restonerecipeadder";
@@ -17,8 +19,8 @@ public class ReStoneRecipeAdder
     @EventHandler	
     public void postInit(FMLPostInitializationEvent event) {
 		
-    	GameRegistry.addRecipe(new ItemStack(IC2Items.getItem("reinforcedStone").getItem(),8), "aaa", "aba", "aaa",
-				'a', new ItemStack(Blocks.stone),
-				'b', IC2Items.getItem("advancedAlloy"));
+    	GameRegistry.addRecipe(new ItemStack(Items.getItem("reinforcedStone").getItem(),8), "aaa", "aba", "aaa",
+				'a', new ItemStack(Block.stone),
+				'b', Items.getItem("advancedAlloy"));
     }
 }
